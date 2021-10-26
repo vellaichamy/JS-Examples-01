d1f48db88e8cb05dca63c9430311a5c38edea504
vellaichamy <uvellaichamy@gmail.com>
2021-10-26 21:31:46 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 // Import stylesheets
 import './style.css';
-let test ='test'
+
 
 let x = 10;
 function a(){
   x=x+5;
 }
 function b(){
-   console.log('x=====>',x);
+   console.log('x ==>',x);
    return x;
 }
 a()
 let xValue = b();
-// Write Javascript code!
+// ======================
+
+function test(){
+  return {
+     a:5
+  };
+}
+let obj = test();
+console.log('obj ==>',obj); 
+// ========================
+
+function sum(...nums){
+  console.log('sum ==>',nums);
+  return nums;  
+}
+sum(4,5);
+const Sum = sum(5,6,7,8);
+
+// =======================
+
+let arr1 = [1,2,3,4,5];
+let arr2 = [...arr1,6,7,8];
+console.log(arr1);
+
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Examples</h1>xValue =`+xValue+`\n`;
+appDiv.innerHTML = `<h1>JS Examples</h1>xValue =`+xValue+`<br> obj = `
++ JSON.stringify(obj) +`<br> Sum = `
++ JSON.stringify(Sum) 
+;
