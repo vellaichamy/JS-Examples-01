aa9dca1c648e0a9663febee9cb302abf36febf6f
vellaichamy <uvellaichamy@gmail.com>
2021-10-26 21:14:32 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 // Import stylesheets
 import './style.css';
+let test ='test'
 
+let x = 10;
+function a(){
+  x=x+5;
+}
+function b(){
+   console.log('x=====>',x);
+   return x;
+}
+a()
+let xValue = b();
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS Examples</h1>xValue =`+xValue+`\n`;
