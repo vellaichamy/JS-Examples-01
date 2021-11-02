f6d2327bff3003e39281bbddef816478012ce98a
vellaichamy <uvellaichamy@gmail.com>
2021-11-02 23:47:54 +0530

    1 file modified

--- index.js
+++ index.js
@@ -106,35 +106,34 @@ const g1 = gen1();
 g1.next();
 // =================================
 
-let arr10 = [4,7,9];
-let arr11 = ["Hi",true,900];
+let arr10 = [4, 7, 9];
+let arr11 = ['Hi', true, 900];
 console.log(arr10);
 console.log(arr11[0]);
 console.log(arr11.length);
-console.log(arr10[arr10.length-1]);
-
+console.log(arr10[arr10.length - 1]);
 
 // =================================
 
-let arr02 = ["ES5","ES6","ES7","ES8"];
-for(let elem in arr02){
-    console.log(elem);
-}
-
-for(let elem of arr02){
+let arr02 = ['ES5', 'ES6', 'ES7', 'ES8'];
+for (let elem in arr02) {
   console.log(elem);
 }
 
-arr02.forEach((elem,index)=>{
-  console.log(elem,index);
-})
+for (let elem of arr02) {
+  console.log(elem);
+}
 
-let arr03 = ["One","Two","Three","Four","Five"];
+arr02.forEach((elem, index) => {
+  console.log(elem, index);
+});
 
-console.log(arr03.push("Six"));
+let arr03 = ['One', 'Two', 'Three', 'Four', 'Five'];
+
+console.log(arr03.push('Six'));
 console.log(arr03);
 
-console.log(arr03.unshift("Test"));
+console.log(arr03.unshift('Test'));
 console.log(arr03);
 
 console.log(arr03.pop());
@@ -146,40 +145,79 @@ console.log(arr03);
 console.log(arr03.splice(2));
 console.log(arr03);
 
-let arr04=["One","Two","One","Three","Four","One","Five"];
-console.log(arr04.indexOf("One"));
-console.log(arr04.indexOf("One",3));
-console.log(arr04.indexOf("one"));
+let arr04 = ['One', 'Two', 'One', 'Three', 'Four', 'One', 'Five'];
+console.log(arr04.indexOf('One'));
+console.log(arr04.indexOf('One', 3));
+console.log(arr04.indexOf('one'));
 
+console.log(arr04.lastIndexOf('One'));
+console.log(arr04.lastIndexOf('One', 3));
 
-console.log(arr04.lastIndexOf("One"));
-console.log(arr04.lastIndexOf("One",3));
-
-console.log(arr.includes("Three",));
+console.log(arr.includes('Three'));
 // ==============================
 
-let arr05=[2,3,6,4,5];
-arr05.map((elem,index)=>{
-    console.log(elem,index);
+let arr05 = [2, 3, 6, 4, 5];
+arr05.map((elem, index) => {
+  console.log(elem, index);
 });
 
-let nArr = arr05.map((elem)=>{
-  
+let nArr = arr05.map((elem) => {
   return Math.sqrt(elem);
-})
+});
 console.log(nArr);
 
-let result = arr05.reduce((accumulator,current)=>{
-  return accumulator+current;
-},10);
+let result = arr05.reduce((accumulator, current) => {
+  return accumulator + current;
+}, 10);
 console.log(result);
 
-let arr06=[4];
-let [c,d=0]=arr06;
-console.log(a,b); 
+let arr06 = [4];
+let [c, d = 0] = arr06;
+console.log(a, b);
 // =================================
 
+var arrayList = ['a', 'b', 'c', 'd', 'e', 'f']; // Created array
+var anotherArrayList = arrayList; // Referenced arrayList by another variable
+arrayList = []; // Empty the array
+console.log(anotherArrayList); // Output ['a', 'b', 'c', 'd', 'e', 'f']
 
+var arrayList = ['a', 'b', 'c', 'd', 'e', 'f']; // Created array
+var anotherArrayList = arrayList; // Referenced arrayList by another variable
+arrayList.length = 0; // Empty the array by setting length to 0
+console.log(anotherArrayList); // Output []
+var arrayList = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+arrayList.splice(0, arrayList.length);
+console.log(anotherArrayList); // Output []
+
+var arrayList = ['a', 'b', 'c', 'd', 'e', 'f']; // Created array
+var anotherArrayList = arrayList; // Referenced arrayList by another variable
+arrayList.splice(0, arrayList.length); // Empty the array by setting length to 0
+console.log(anotherArrayList); // Output []
+
+var arrayList = ['a', 'b', 'c', 'd', 'e', 'f'];
+while (arrayList.length) {
+  arrayList.pop();
+}
+console.log(anotherArrayList); // Output []
+// check if an object is an array or not
+var arrayList = [1, 2, 3];
+
+if (Object.prototype.toString.call(arrayList) === '[object Array]') {
+  console.log('Array!');
+}
+
+console.log(Array.isArray(arrayList));
+// ================================
+
+var x1 = 1;
+var output = (function () {
+  // delete x1;
+  return x1;
+})();
+
+// output 1 as output. delete operator is used to delete a property from an object. Here x is not an object it's global variable of type number.
+console.log(output);
 
 const appDiv = document.getElementById('app');
 appDiv.innerHTML =
